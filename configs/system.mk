1b77b6e4f410a27be02c5f68878278a012eb99d2
xilence2210 <xilence2210@gmail.com>
2014-03-26 17:41:09 +0800

    Prebuilt: Add RootExplorer
    Change-Id: I665af3d8a52641f946e14cb2fa7215d4b7592811

--- configs/system.mk
+++ configs/system.mk
@@ -39,7 +39,8 @@ PRODUCT_COPY_FILES += \
     vendor/$(VENDOR)/prebuilt/bin/su:system/xbin/daemonsu \
     vendor/$(VENDOR)/prebuilt/bin/su:system/xbin/su \
     vendor/$(VENDOR)/prebuilt/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon \
-    vendor/$(VENDOR)/prebuilt/apk/Superuser.apk:system/app/Superuser.apk
+    vendor/$(VENDOR)/prebuilt/apk/Superuser.apk:system/app/Superuser.apk \
+    vendor/$(VENDOR)/prebuilt/apk/RootExplorer.apk:system/app/RootExplorer.apk
 
 # OTA Support
 PRODUCT_COPY_FILES += \
