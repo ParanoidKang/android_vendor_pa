b701f31e9827c475280d089273343d8bedfc2f84
xilence2210 <xilence2210@gmail.com>
2014-04-07 16:37:03 +0800

    add hammerhead support
    Change-Id: I1839f5bd996f1dbc25f35df36ffa67c4bfa8223a

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -63,6 +63,9 @@ endif
 ifeq (pa_grouper,$(TARGET_PRODUCT))
     PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_grouper.mk
 endif
+ifeq (pa_hammerhead,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_hammerhead.mk
+endif
 ifeq (pa_hercules,$(TARGET_PRODUCT))
     PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_hercules.mk
 endif
