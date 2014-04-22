441ffa2a2593259b2bb1efc714da2da3e29974c2
xilence2210 <xilence2210@gmail.com>
2014-04-22 13:44:46 +0800

    fix missing  geehrc support
    Change-Id: I8db5d0c17a0853eba3b550356e049021bb67ed02

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,5 @@
 add_lunch_combo pa_gee-userdebug
+add_lunch_combo pa_geehrc-userdebug
 add_lunch_combo pa_grouper-userdebug
 add_lunch_combo pa_d802-userdebug
 add_lunch_combo pa_hammerhead-userdebug
