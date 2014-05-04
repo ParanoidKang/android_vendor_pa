d85dd5c3bca8e3d71f6c65a340ee91cb1791961d
xxxifan <445947962@qq.com>
2014-05-04 22:30:00 +0800

    add mako support
    Change-Id: I59b5191c48ddb626d568234e1abf8941d5500e87

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -126,6 +126,9 @@ endif
 ifeq (pa_maguro,$(TARGET_PRODUCT))
     PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_maguro.mk
 endif
+ifeq (pa_mako,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_mako.mk
+endif
 ifeq (pa_m7spr,$(TARGET_PRODUCT))
     PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_m7spr.mk
 endif
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,3 +4,4 @@ add_lunch_combo pa_grouper-userdebug
 add_lunch_combo pa_d802-userdebug
 add_lunch_combo pa_hammerhead-userdebug
 add_lunch_combo pa_lu6200-userdebug
+add_lunch_combo pa_mako-userdebug
